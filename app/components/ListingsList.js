3e95bb85770d2c479a49b339cffdf564b687ab4e
BenGoldstein88 <bengoldsteinmusic@gmail.com>
2016-09-18 19:37:40 -0700

    allow for both searches via axios promises

--- app/components/ListingsList.js
+++ app/components/ListingsList.js
@@ -8,31 +8,60 @@ export default class ListingsList extends React.Component {
     super(props);
 
     this.state= {
+    	auction: {},
     	listings: []
     }
 
     this.getListings = this.getListings.bind(this)
+    this.getUserListings = this.getUserListings.bind(this)
   }
 
   getListings() {
+  	var that = this
+
   	axios({
   		method: 'get',
   		url: 'http://localhost:3000/currentauction',
-  		headers: { 'Authorization': this.props.authToken}
+  		headers: { 'Authorization': that.props.authToken}
   	})
   	.then(function(response) {
   		console.log('Response: ', response)
+  		that.setState({
+  			auction: response.data.auction,
+  			listings: response.data.listings
+  		})
   	})
   	.catch(function(error) {
   		console.log('Error: ', error)
   	})
   }
 
+  getUserListings() {
+  	var that = this
+  	axios({
+  		method: 'get',
+  		url: 'http://localhost:3000/userlistings',
+  		headers: { 'Authorization': that.props.authToken}
+  	})
+  	.then(function(response) {
+  		console.log('Response: ', response)
+  		that.setState({
+  			listings: response.data.listings
+  		})
+  	})
+  	.catch(function(error) {
+  		console.log('Error: ', error)
+  	})  	
+  }
+
   render() {
     return (
       <div>
       <form onSubmit={this.getListings} >
-      	<input type='submit' value='get listings' />
+      	<input type='submit' value='Current Auction' />
+      </form>
+      <form onSubmit={this.getUserListings} >
+      	<input type='submit' value='Your Listings' />
       </form>
       </div>
     );
